8b4cbe5efeded804cfc86cb5459b9ccf5c3b2b66
Yasei no Koushiki <b65913jl@gmail.com>
2025-10-07 10:12:44 +0900

    std::list

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include<iostream>
 
+#include"GetStations.h"
+
+template<uint32_t Year>
+void PrintStations() {
+	std::cout << Year << "年\n--------------------\n";
+	std::list<char const*> stations;
+	GetStations<Year>(stations);
+	uint32_t no{ 0U };
+	for (auto const& station : stations) {
+		std::printf("%2u | %s\n", ++no, station);
+	}
+}
+
 int main() {
+	PrintStations<1970U>();
+	std::cout << "\n";
+	PrintStations<2019U>();
+	std::cout << "\n";
+	PrintStations<2022U>();
 	return 0;
 }
