fe7cffbc6c9e5225e9d365db55a0e395f6128bfa
Yasei no Koushiki <b65913jl@gmail.com>
2026-01-16 12:21:13 +0900

    std::thread

--- main.cpp
+++ main.cpp
@@ -1,3 +1,31 @@
+#include<iostream>
+#include<format>
+#include<vector>
+#include<thread>
+#include<semaphore>
+
 int main() {
+	int idx{ 0 };
+	std::binary_semaphore binSem{ 1LL };
+
+	auto print{
+		[&] () {
+			binSem.acquire();
+			++idx;
+			std::cout << std::format("thread {}\n", idx);
+			binSem.release();
+		}
+	};
+
+	std::vector<std::thread> ths{};
+
+	for (int i{ 0 }; i < 3; ++i) {
+		ths.emplace_back(print);
+	}
+	
+	for (auto& th : ths) {
+		th.join();
+	}
+
 	return 0;
 }
